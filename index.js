88449a6a7963207fa9c4156a0f120a4b260273c2
jSSjin <jerat004@gmail.com>
2025-08-14 01:13:53 +0700

    trasfer db to db.js cannot connnect yet denied access problem

--- index.js
+++ index.js
@@ -1,47 +1,12 @@
 const express = require('express');
 
-const mysql = require('mysql2');
-
-const dotenv = require('dotenv');
-
 const app = express();
 
 const PORT = 3000;
 
-
-
-dotenv.config();
-
-
-
-const db = mysql.createConnection({
-
-          host: process.env.DB_HOST,
-
-          port: process.env.DB_PORT,
-
-          user: process.env.DB_USER,
-
-          password: process.env.DB_PASSWORD,
-
-          database: process.env.DB_DATABASE,
-
-});
-
-
-
-db.connect(err => {
-
-          if (err) throw err;
-
-          console.log('✅ Connected to MySQL');
-
-});
-
-
+const db = require('./config/db')
 
 app.use(express.json());
-
 
 
 // Root test
